c37b365106b7c395f14d1e070ef5259f9dbd5230
JulioV10 <julio.vargas11@hotmail.com>
2023-03-10 00:25:27 +0000

    landing page project

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Card = ()=>{
+    return(
+        <>
+        <div className="card" style={{width: "18rem"}}>
+        <img src="https://nupec.com/wp-content/uploads/2020/07/Captura-de-pantalla-2020-07-24-a-las-17.33.44.png" className="card-img-top" alt="..." />
+        <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+        </div></>
+    )}
+export default Card 
